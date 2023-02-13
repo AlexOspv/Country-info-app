89c8b66e9786b4757a96991a8b1a9a18b2970596
AlexOspv <lesha_osipov@mail.ru>
2023-02-13 16:01:25 +0300

    activity_main layout created. View Binding implemented

--- app/src/main/java/com/alexos/countryinfo/MainActivity.kt
+++ app/src/main/java/com/alexos/countryinfo/MainActivity.kt
@@ -2,10 +2,13 @@ package com.alexos.countryinfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alexos.countryinfo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
